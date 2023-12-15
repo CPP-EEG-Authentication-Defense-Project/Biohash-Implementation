494cff2b9127f9c7ef633e896f8ebefd3996269e
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2023-12-15 11:01:18 -0800

    Move shape check to base class

--- src/auth_biohash/binary_encoding/threshold.py
+++ src/auth_biohash/binary_encoding/threshold.py
@@ -1,15 +1,13 @@
-import enum
 import io
 
 from . import base
-from .. import exceptions
 
 
 class ThresholdBinaryEncoder(base.BinaryEncoder):
     def __init__(self, threshold: float):
         self.threshold = threshold
 
-    def encode(self, data):
+    def convert_to_binary(self, data):
         """
         Converts the given 1-D data array into a binary string, using the current threshold to determine when to use
         a '0' or a '1' in the result.
@@ -17,8 +15,6 @@ class ThresholdBinaryEncoder(base.BinaryEncoder):
         :param data: the data to binarize.
         :return: the resulting binary string.
         """
-        if data.ndim > 1:
-            raise exceptions.BinarizationException(f'Expected data to be a 1D array, got {data.shape}')
         with io.StringIO() as stream:
             for element in data:
                 if element <= self.threshold:
