c4487730c5cf08973a92f79337e3edae8c6fff21
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-03-30 14:51:35 -0700

    Update hash generation and comparison

--- src/auth_biohash/hash.py
+++ src/auth_biohash/hash.py
@@ -1,4 +1,5 @@
 import numpy as np
+import typing
 from feature_encoding import base
 
 from . import random_token, exceptions
@@ -48,36 +49,49 @@ class BioHash:
     """
     Core BioHash class representing a single hash instance.
     """
-    def __init__(self, content: str, validation_threshold: float):
-        if validation_threshold < 0 or validation_threshold > 1:
-            raise ValueError('validation_threshold must be between 0 and 1')
+    def __init__(self, content: str):
         self.content = content
-        self.validation_threshold = validation_threshold
 
     @classmethod
     def generate_hash(cls,
                       features: np.ndarray,
-                      validation_threshold: float,
+                      token: typing.Union[int, float, str],
                       encoder: base.BinaryEncoder) -> 'BioHash':
         """
         Generates a BioHash instance using the provided key data components.
 
         :param features: the features to use to generate the hash.
-        :param validation_threshold: the threshold to use in the generated BioHash instance.
+        :param token: A user-specific token to use during hash generation.
         :param encoder: a feature encoder to use to encode the feature data into a binary string.
         :return: the BioHash instance.
         """
-        binary_data = encoder.encode(features)
-        return cls(binary_data, validation_threshold)
+        matrix_generator = random_token.MatrixGenerator(token)
+        normalizer = TokenMatrixNormalization(matrix_generator)
+        normalized_features = normalizer.normalize(features)
+        binary_data = encoder.encode(normalized_features)
+        return cls(binary_data)
+
+    @staticmethod
+    def compare(hash_a: 'BioHash', hash_b: 'BioHash') -> float:
+        """
+        Compares two BioHash instances, returning a float number between 0 and 1 (inclusive) that represents
+        the percentage difference between the two hashes.
+        This similarity is based on hamming distance,
+        in a manner described in https://doi.org/10.1109/TIFS.2022.3204222.
+
+        :param hash_a: The first hash to compare.
+        :param hash_b: The second hash to compare.
+        :return: The percentage difference between the two hashes.
+        """
+        xor_result = int(str(hash_a), 2) ^ int(str(hash_b), 2)
+        xor_bin = '{0:b}'.format(xor_result)
+        bits_changed = sum([int(bit) for bit in xor_bin])
+        max_len = max(len(hash_a), len(hash_b))
+        percentage_change = bits_changed / max_len
+        return percentage_change
 
     def __str__(self):
         return str(self.content)
 
-    def __eq__(self, other):
-        if not isinstance(other, BioHash):
-            return False
-        xor_result = int(str(self), 2) ^ int(str(other), 2)
-        xor_bin = '{0:b}'.format(xor_result)
-        bits_changed = sum([int(bit) for bit in xor_bin])
-        percentage_change = bits_changed / len(str(self))
-        return percentage_change <= self.validation_threshold
+    def __len__(self):
+        return len(self.content)
